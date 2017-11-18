7a00b2fb9c15dedeb1ae727d9c134c4cc8e7ad55
Sparky1205 <adamovich2010@yandex.ru>
2017-11-18 12:13:43 +0300

    Add files via upload

---
+++ GOST ver 1.0/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GOST_ver_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.isParallel = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(202, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(174, 187);
+            this.keyTextBox.Multiline = true;
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(183, 64);
+            this.keyTextBox.TabIndex = 4;
+            // 
+            // isParallel
+            // 
+            this.isParallel.AutoSize = true;
+            this.isParallel.Location = new System.Drawing.Point(126, 286);
+            this.isParallel.Name = "isParallel";
+            this.isParallel.Size = new System.Drawing.Size(213, 21);
+            this.isParallel.TabIndex = 5;
+            this.isParallel.Text = "Паралелльные вычисления";
+            this.isParallel.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Кодировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(320, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 27);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Декодировать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Исходный файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Сохраненный файл";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ключ шифрования";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 417);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.isParallel);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GOST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.CheckBox isParallel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GOST ver 1.0/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace GOST_ver_1._0
+{
+    public partial class Form1 : Form 
+    {
+        byte[,] SubstitutionBox = new byte[,] {
+            {4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3},
+            {14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9},
+            {5, 8, 1, 13, 10, 3, 4, 2, 14, 15, 12, 7, 6, 0, 9, 11},
+            {7, 13, 10, 1, 0, 8, 9, 15, 14, 4, 6, 12, 11, 2, 5, 3},
+            {6, 12, 7, 1, 5, 15, 13, 8, 4, 10, 9, 14, 0, 3, 11, 2},
+            {4, 11, 10, 0, 7, 2, 1, 13, 3, 6, 8, 5, 9, 12, 15, 14},
+            {13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12},
+            {1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12}
+            };
+
+        //int[] keyMap = { 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 0 };
+        string file;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            Form1 form = new Form1();
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "D:\\";
+            openFileDialog1.Filter = "Rar (*.rar)|*.rar";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                file = openFileDialog1.FileName;
+               
+            }
+            
+
+        }
+
+        private byte[] EncodeBlock(byte[] block, uint[] keys)
+        {
+            // separate on 2 blocks.
+            uint N1 = BitConverter.ToUInt32(block, 0);
+            uint N2 = BitConverter.ToUInt32(block, 4);
+
+            for (int i = 0; i < 32; i++)
+            {
+                int keyIndex = i < 24 ? (i % 8) : (7 - i % 8); // to 24th cycle : 0 to 7; after - 7 to 0;
+                var s = (N1 + keys[keyIndex]) % uint.MaxValue; // (N1 + X[i]) mod 2^32
+                s = Substitution(s); // substitute from box
+                s = (s << 11) | (s >> 21);
+                s = s ^ N2; // ( s + N2 ) mod 2
+                //N2 = N1;
+                //N1 = s;
+                if (i < 31) // last cycle : N1 don't change; N2 = s;
+                {
+                    N2 = N1;
+                    N1 = s;
+                }
+                else
+                {
+                    N2 = s;
+                }
+            }
+
+            var output = new byte[8];
+            var N1buff = BitConverter.GetBytes(N1);
+            var N2buff = BitConverter.GetBytes(N2);
+
+            for (int i = 0; i < 4; i++)
+            {
+                output[i] = N1buff[i];
+                output[4 + i] = N2buff[i];
+            }
+
+            return output;
+        }
+
+        byte[] Encode(byte[] data, byte[] key, bool isParallel = false)
+
+        {
+            var subkeys = GenerateKeys(key);
+            var result = new byte[data.Length];
+            var block = new byte[8];
+
+            if (isParallel)
+            {
+                Parallel.For(0, data.Length / 8, i =>
+                {
+                    Array.Copy(data, 8 * i, block, 0, 8);
+                    Array.Copy(EncodeBlock(block, subkeys), 0, result, 8 * i, 8);
+                });
+            }
+            else
+            {
+                for (int i = 0; i < data.Length / 8; i++) // N blocks 64bits length.
+                {
+                    Array.Copy(data, 8 * i, block, 0, 8);
+                    Array.Copy(EncodeBlock(block, subkeys), 0, result, 8 * i, 8);
+                }
+            }
+            return result;
+        }
+
+        protected uint[] GenerateKeys(byte[] key)
+        {
+            if (key.Length != 32)
+            {
+                throw new Exception("Wrong key.");
+            }
+
+            var subkeys = new uint[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                subkeys[i] = BitConverter.ToUInt32(key, 4 * i);
+            }
+
+            return subkeys;
+        }
+
+        protected uint Substitution(uint value)
+        {
+            
+
+            uint output = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                var temp = (byte)((value >> (4 * i)) & 0x0f);
+                temp = SubstitutionBox[i,temp];
+                output |= (UInt32)temp << (4 * i);
+            }
+
+            return output;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = File.ReadAllBytes(@file);
+
+
+
+            byte[] res = Encode(bytes, Encoding.Default.GetBytes(keyTextBox.Text), isParallel.Checked);
+            /*
+                        FileStream fileStream = new FileStream(textBox2.Text, FileMode.Create);
+
+                        for (int i = 0; i < res.Length; i++)
+                        {
+                            fileStream.WriteByte(res[i]);
+                        }*/
+            sav(res);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "Rar (.*rar)|*.rar";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    textBox2.Text = saveFileDialog1.FileName;
+                    
+                    myStream.Close();
+                }
+            }
+        }
+
+        byte[] Decode(byte[] data, byte[] key, bool isParallel = false)
+        {
+            var subkeys = GenerateKeys(key);
+            var result = new byte[data.Length];
+            var block = new byte[8];
+
+            if (isParallel)
+            {
+                Parallel.For(0, data.Length / 8, i =>
+                {
+                    Array.Copy(data, 8 * i, block, 0, 8);
+                    Array.Copy(DecodeBlock(block, subkeys), 0, result, 8 * i, 8);
+                });
+            }
+            else
+            {
+                for (int i = 0; i < data.Length / 8; i++) // N blocks 64bits length.
+                {
+                    Array.Copy(data, 8 * i, block, 0, 8);
+                    Array.Copy(DecodeBlock(block, subkeys), 0, result, 8 * i, 8);
+                }
+            }
+            return result;
+        }
+
+        private byte[] DecodeBlock(byte[] block, uint[] keys)
+        {
+            // separate on 2 blocks.
+            uint N1 = BitConverter.ToUInt32(block, 0);
+            uint N2 = BitConverter.ToUInt32(block, 4);
+
+            for (int i = 0; i < 32; i++)
+            {
+                int keyIndex = i < 8 ? (i % 8) : (7 - i % 8); // to 24th cycle : 0 to 7; after - 7 to 0;
+                var s = (N1 + keys[keyIndex]) % uint.MaxValue; // (N1 + X[i]) mod 2^32
+                s = Substitution(s); // substitute from box
+                s = (s << 11) | (s >> 21);
+                s = s ^ N2;
+                if (i < 31) // last cycle : N1 don't change; N2 = s;
+                {
+                    N2 = N1;
+                    N1 = s;
+                }
+                else
+                {
+                    N2 = s;
+                }
+            }
+
+            var output = new byte[8];
+            var N1buff = BitConverter.GetBytes(N1);
+            var N2buff = BitConverter.GetBytes(N2);
+
+            for (int i = 0; i < 4; i++)
+            {
+                output[i] = N1buff[i];
+                output[4 + i] = N2buff[i];
+            }
+
+            return output;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = File.ReadAllBytes(@file);
+
+
+
+            byte[] res = Decode(bytes, Encoding.Default.GetBytes(keyTextBox.Text), isParallel.Checked);
+            /*
+                        FileStream fileStream = new FileStream(textBox2.Text, FileMode.Create);
+
+                        for (int i = 0; i < res.Length; i++)
+                        {
+                            fileStream.WriteByte(res[i]);
+                        }
+
+                        MessageBox.Show("Готово!");*/
+            sav(res);
+        }
+
+        void sav (byte[] res)
+        {
+            using (FileStream fstream = new FileStream(textBox2.Text, FileMode.OpenOrCreate))
+            {
+      
+                fstream.Write(res, 0, res.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+        }
+    }
+    }
+
+
